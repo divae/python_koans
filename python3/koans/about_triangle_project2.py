1349b41191ae5f0b23719727d240c1ce17717053
Estela Medrano Jiménez <stlmedrano@gmail.com>
2020-07-30 10:15:22 +0200

    Added About Triangle Project2

--- python3/koans/about_triangle_project2.py
+++ python3/koans/about_triangle_project2.py
@@ -6,7 +6,35 @@ from runner.koan import *
 # You need to finish implementing triangle() in the file 'triangle.py'
 from .triangle import *
 
+
+def triangle(a, b, c):
+    if sides_less_than_one(a, b, c) or sum_of_any_two_sides_less_than_the_third(a, b, c):
+        raise TriangleError("My Message")
+    elif all_slides_equals(a, b, c):
+        return 'equilateral'
+    elif two_slides_equals(a, b, c):
+        return 'isosceles'
+    else:
+        return 'scalene'
+
+def all_slides_equals(a, b, c):
+    if a == b == c:
+        return True
+
+def two_slides_equals(a, b, c):
+    if a == b or b == c or c == a:
+        return True
+
+def sides_less_than_one(a, b, c):
+    if a < 1 or b < 1 or c < 1:
+        return True
+
+def sum_of_any_two_sides_less_than_the_third(a, b, c):
+    if ((a + b) > c) or ((b + c) > a) or ((c + a) > b):
+        return True
+
 class AboutTriangleProject2(Koan):
+
     # The first assignment did not talk about how to handle errors.
     # Let's handle that part now.
     def test_illegal_triangles_throw_exceptions(self):
